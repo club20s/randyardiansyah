de58695351dc37805a26165db0d9ed568df3ab2e
warenbergg <warenbergg99@gmail.com>
2026-04-08 22:47:47 +0800

    fix missing projects section 2x

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder="templates", static_folder="static")
-
+app = Flask(__name__)
 
 # ============================================
 # CASE STUDIES DATA
